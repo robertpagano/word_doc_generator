04f2a5c1f5be7e09c5a3d3d8acd49e438465fe1c
Robert Pagano <robert.pagano@thespurgroup.com>
2020-08-19 23:16:11 -0700

    replaced old make_doc_datagrame, added new todo notes, and started updating make_master_file

--- word_doc_gen.py
+++ word_doc_gen.py
@@ -14,14 +14,13 @@ from docxcompose.composer import Composer
 from pathlib import Path
 
 # %%
-## This version extracts all needed data from filepaths list and doc list, directly from Flow API call
+# this version extracts all needed data from filepaths list and doc list, directly from Flow API call
 
-## Below is the final version of accessing files
-## - It takes in 2 lists:
-##  + filepaths_to_docs
-##  + filepaths_to_summs
-## - It will need to go inside of make_master_file, which is the file that will be used
-## in the api
+# below is the final version of accessing files
+# - it takes in 2 lists:
+#  + filepaths_to_docs
+#  + filepaths_to_summs
+# - it will need to go inside of make_master_file, which is the file that will be used in the api
 
 filepaths_to_docs = glob.glob('articles/**/*.docx')
 filepaths_to_summs = glob.glob('articles/**/*.txt')
@@ -92,84 +91,7 @@ def make_doc_dataframe(filepaths_to_docs, filepaths_to_summs):
 
 df = make_doc_dataframe(filepaths_to_docs, filepaths_to_summs)
 
-df
-# %%
-
-def make_doc_dataframe_from_lists(filepaths_list, doc_list, summ_list_txt):
-    '''
-    new version - this will now take in a list of paths, and a list of files. DF will have the following fields:
-
-    filename: filename ending with docx
-    article_title: filename minus .docx
-    section:
-    month:
-    doc_object: pulled from other list
-
-    '''
-
-    cols = ['filename', 'article_name', 'section', 'month', 'doc_object', 'summ_object', 'filepath']
-
-    temp_list = []
-
-    # each list will be [filename, article_name, section, month, doc_object, summ_object, filepath] for one path, 
-    # then be appended to dataframe
-
-    df = pd.DataFrame(columns=cols)
-
-    zip_dict = dict(zip(filepaths_list, zip(doc_list, summ_list_txt)))
-
-    # print(zip_dict)
-
-    for path, docs in zip_dict.items():
-        
-        filename = path.split('/')[-1]
-        article_name = filename[:-5]
-        section = path.split('/')[-2]
-        month = path.split('/')[-3]
-        temp_list.append(filename)
-        temp_list.append(article_name)
-        temp_list.append(section)
-        temp_list.append(month)
-        temp_list.append(docs[0])
-        temp_list.append(docs[1])
-        temp_list.append(path)
-        temp_series = pd.Series(temp_list, index = df.columns)
-        df = df.append(temp_series, ignore_index = True)
-        temp_list = []
-
-    # # 'section_order' is the order we want the sections to be in the final document
-    # # this sorts the dataframe by a specific order in sections    
-    section_order = ["Content + Training", "Product + Availability", "Programs + Offers", "Partner Update", "nocat"]
-    df['section'] = pd.Categorical(df['section'], section_order)
-
-    df = df.sort_values('section')
-
-    df['new_section'] = df['section'].shift().fillna('nocat') != df['section']
-    # df['prev_section'] = df['prev_section'].fillna('nocat')
-    # df['new_section'] = df['prev_section'] != df['section']
-
-    
-    return df
-
-df = make_doc_dataframe_from_lists(filepaths_list, doc_list, summ_list_txt)
-
-# %%
-df.head(50)
-# %%
 '''
-now I just need to make the document! look at word_doc_gen_v3 "make_master_file" etc.
-
-for summary, try using actual code rather than calling API (but it may make sense to call API anyway)
-
-- summary goes above full article, and call it abstract
-
-  - if time, also code up the two different files
-
-- clean up summaries - make sure to not grab title
-
-- make logic to split up by section, and put divider images between the sections
-
-  - try to make the divider images heading 1, and article names heading 2
   - make a document for each heading that lives in the repo, can pull from these using composer to add to the newsletter
 
 - use font information that maria sent
@@ -184,13 +106,40 @@ for summary, try using actual code rather than calling API (but it may make sens
 
 - make sure in landscape mode (sideways)
 
-- once all done, let's update flask api 2 repo, and azure repo
-
-- header image (maria mentioned she had code for that, take a look at her old word doc file to see)
 '''
+# %%
+def make_toc(doc):
+    '''
+    this function creates a table of contents object within a docx object, which will be called when the master files are created
 
-# to test this, i'll need to make a list of real docs, and real summary docs
-# so actually, we receive 3 lists
+    it indexes any text with "heading styles"
+
+    if article "sections" are heading 1, and article titles are heading 2, it will take care of all of the proper formatting/indenting
+    '''
+    paragraph = doc.add_paragraph()
+    run = paragraph.add_run()
+    fldChar = OxmlElement('w:fldChar')  # creates a new element
+    fldChar.set(qn('w:fldCharType'), 'begin')  # sets attribute on element
+    instrText = OxmlElement('w:instrText')
+    instrText.set(qn('xml:space'), 'preserve')  # sets attribute on element
+    instrText.text = 'TOC \\o "1-3" \\h \\z \\u'   # change 1-3 depending on heading levels you need
+
+    fldChar2 = OxmlElement('w:fldChar')
+    fldChar2.set(qn('w:fldCharType'), 'separate')
+    fldChar3 = OxmlElement('w:t')
+    fldChar3.text = "Right-click to update field."
+    fldChar2.append(fldChar3)
+
+    fldChar4 = OxmlElement('w:fldChar')
+    fldChar4.set(qn('w:fldCharType'), 'end')
+
+    r_element = run._r
+    r_element.append(fldChar)
+    r_element.append(instrText)
+    r_element.append(fldChar2)
+    r_element.append(fldChar4)
+    p_element = paragraph._p
+    return doc
 
 # %%
 def create_doc(doc, summ_object, section, article_name, new_section = False):
@@ -261,8 +210,11 @@ def create_doc(doc, summ_object, section, article_name, new_section = False):
 
     return doc
 
-# %%
 
+# %%
+'''
+SAVE FOR POTENTIAL FUTURE TESTING
+'''
 # test_doc = Document('Quick.docx')
 # summ_object = 'fake summary'
 # section = 'Partner Update'
@@ -290,40 +242,8 @@ def create_doc(doc, summ_object, section, article_name, new_section = False):
 # so the above doesnt seem to work on a single doc, but the other one didnt either
 # hopefully it works when they all get smashed together, we will see!
 
-def make_toc(doc):
-    '''
-    this function creates a table of contents object within a docx object, which will be called when the master files are created
-
-    it indexes any text with "heading styles"
-
-    if article "sections" are heading 1, and article titles are heading 2, it will take care of all of the proper formatting/indenting
-    '''
-    paragraph = doc.add_paragraph()
-    run = paragraph.add_run()
-    fldChar = OxmlElement('w:fldChar')  # creates a new element
-    fldChar.set(qn('w:fldCharType'), 'begin')  # sets attribute on element
-    instrText = OxmlElement('w:instrText')
-    instrText.set(qn('xml:space'), 'preserve')  # sets attribute on element
-    instrText.text = 'TOC \\o "1-3" \\h \\z \\u'   # change 1-3 depending on heading levels you need
-
-    fldChar2 = OxmlElement('w:fldChar')
-    fldChar2.set(qn('w:fldCharType'), 'separate')
-    fldChar3 = OxmlElement('w:t')
-    fldChar3.text = "Right-click to update field."
-    fldChar2.append(fldChar3)
-
-    fldChar4 = OxmlElement('w:fldChar')
-    fldChar4.set(qn('w:fldCharType'), 'end')
-
-    r_element = run._r
-    r_element.append(fldChar)
-    r_element.append(instrText)
-    r_element.append(fldChar2)
-    r_element.append(fldChar4)
-    p_element = paragraph._p
-    return doc
-# %% 
-def make_master_file(df, master_filename):
+# %%
+def make_master_file(filepaths_to_docs, filepaths_to_summs):
 
     ## THIS NEEDS TO TAKE TWO LISTS FROM API
     '''
@@ -336,6 +256,9 @@ def make_master_file(df, master_filename):
         - formats headings and makes a table of contents for each file
 
     '''
+    df = make_doc_dataframe(filepaths_to_docs, filepaths_to_summs)
+
+
     toc = Document()
     paragraph = toc.add_paragraph('TABLE OF CONTENTS')
     toc = make_toc(toc)
@@ -368,12 +291,63 @@ def make_master_file(df, master_filename):
     for document in article_list[1:]:
         composer.append(document)
 
-    composer.save(master_filename) 
+    composer.save(master_filename)
+
+    return master_doc 
+
+
+# %% 
+# def make_master_file(df, master_filename):
+
+#     ## THIS NEEDS TO TAKE TWO LISTS FROM API
+#     '''
+#     needs to take in the dictionary, parse out the variables, and change the calls
+#     to the functions to include those variables
+
+#     it uses the above functions to:
+    
+#         - create the summaries for the text in each individual article
+#         - formats headings and makes a table of contents for each file
+
+#     '''
+#     toc = Document()
+#     paragraph = toc.add_paragraph('TABLE OF CONTENTS')
+#     toc = make_toc(toc)
+#     article_list = [toc]
+
+#     # for k, v in doc_dict.items():
+#     #     doc = v['doc']
+#     #     month = v['month']
+#     #     section = v['section']
+#     #     article_name = k
+#     #     article_list.append(create_doc(doc, month, section, article_name, summ))
+
+#     for index, row in df.iterrows():
+#         # first set all new section to FALSE, then test it and come back to add logic ot change this
+#         doc = row['doc_object']
+#         summ_object = row['summ_object']
+#         section = row['section']
+#         article_name = row['article_name']
+
+#         #check if article is first in new section, which would make us start a new section heading
+        
+#         if row['new_section'] == True:
+#             article_list.append(create_doc(doc, summ_object, section, article_name, new_section = True))
+
+#         else:
+#             article_list.append(create_doc(doc, summ_object, section, article_name, new_section = False))
+
+#     master = article_list[0]
+#     composer = Composer(master)
+#     for document in article_list[1:]:
+#         composer.append(document)
+
+#     composer.save(master_filename) 
 
 # %%
 make_master_file(df, 'here_is_my_big_test.docx')
 # %%
-def make_masters(doc_dict, summ_filename, article_filename):
+def make_masters(filepaths_to_docs, filepaths_to_summs):
     '''
     this takes in the dictionary of documents and metadata
 
